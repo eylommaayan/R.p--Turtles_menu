7ae8e5c264893c7cc0d95100c6ed021fb95d1916
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-10-09 20:45:52 +0300

    38.create more component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
 import React from "react";
 
+
+
+
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
-      <Pizza />
+      <Header/>
+      <Menu/>
+      <Footer/>
     </div>
   );
 }
 
+function Header(){
+  return <h1>רכיב פיצה ביוב</h1>
+}
+
+function Menu(){
+  return (
+    <div>
+      <h2>תפריט הפיצה צבי הנינג'ה</h2>
+      <Pizza/>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}.כרגע אנחנו בביוב</footer>
+  )
+}
+
+
 function Pizza() {
-  return <h2>Pizza</h2>;
+  return(
+    <div>
+     <img src="./pizzas/tiramisu.jpg" alt="Pizza" />
+    <h2>Pizza</h2>;
+    <p>פיצה צבי הניג'נג'ה עם  רוטב ביוב</p>
+</div>
+  )
+  
 }
 
 export default App;
